6ee32f1251bc3a4da823c0a1bf8fb9eb70a31115
Daniils Šarpans <166118195+23DP1DSarp@users.noreply.github.com>
2025-03-09 19:09:14 +0000

    Pievienoju grafisko interfeisu

--- src/main/java/lv/rvt/Main.java
+++ src/main/java/lv/rvt/Main.java
@@ -6,18 +6,34 @@ import java.util.*;
 
 public class Main 
 {
-    public static void main( String[] args )
-    {
+
+    // Comma separeted values
+
+   // Izlasīt persons.csv izmantojot BufferedReader objektu
+    
+   public static void main(String[] args) throws Exception{
+           
+        
+
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println("Hello, choose your command: ");
+    System.out.println("singleplayer - start the singleplayer mode");
+    System.out.println("multiplayer - start the multiplayer mode");
+    System.out.println("exit - exit the game\n");
+    
+
+    while (true) {
+
+        String command = scanner.nextLine();
+    
+
+    if (command.equals("singleplayer")) {
         ArrayList<Card> cards = new ArrayList<Card>();
-
         Random random = new Random();
-
-
         String[] colors = {"Green", "Blue", "Yellow", "Red"};
         int[] numbers = {0,1,2,3,4,5,6,7,8,9};
-
         Card card;
-
         for(int i=0; i<10; i++){
 
             card = new Card(colors[random.nextInt(4)], numbers[random.nextInt(10)]);
@@ -26,8 +42,7 @@ public class Main
 
         }
 
-
-        /*  for(int a=0; a < cards.size(); a++){
+         /*  for(int a=0; a < cards.size(); a++){
             
             for(int b=1; b < cards.size(); b++){
                 if (cards.get(a).color.equals(cards.get(b).color) == true) {
@@ -41,7 +56,40 @@ public class Main
             
         }
             */
-            
-        System.out.println(cards);
+
+            System.out.println("");
+            System.out.println(cards);
     }
+    else if (command.equals("multiplayer")) {
+
+        System.out.println("\nComming soon!");
+    
+    }
+    else if (command.equals("exit")) {
+        scanner.close();
+        break;
+    } 
+    else if (command.equals("help")){
+        System.out.println("Hello, choose your command: ");
+        System.out.println("singleplayer - start the singleplayer mode");
+        System.out.println("multiplayer - start the multiplayer mode");
+        System.out.println("exit - exit the game");
+    }
+    
+
+    System.out.println("\nsingleplayer - start the singleplayer mode");
+    System.out.println("multiplayer - start the multiplayer mode");
+    System.out.println("exit - exit the game\n");
 }
+    
+    
+
+    System.out.println("\nBye bye");
+
+
+
+
+
+        }
+
+    }
