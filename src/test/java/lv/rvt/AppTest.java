6fe2e63b2de0d21be8f9fe690aa9794e3f8dd3bf
Daniils Šarpans <166118195+23DP1DSarp@users.noreply.github.com>
2025-05-07 07:49:01 +0000

    Pievienoju testus

--- src/test/java/lv/rvt/AppTest.java
+++ src/test/java/lv/rvt/AppTest.java
@@ -1,20 +1,45 @@
 package lv.rvt;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
+
 
 import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
+
 public class AppTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+   
+    public class SinglePlayerTest {
+
+    private SinglePlayer singlePlayer;
+
+    
+    public void setUp() {
+        singlePlayer = new SinglePlayer();
+        
+        singlePlayer.cards.add(new Card("Red", 5));
+        singlePlayer.computerCards.add(new Card("Red", 3));
+        singlePlayer.computerCards.add(new Card("Green", 7));
     }
+
+    @Test
+    public void testProcessComputerCards_validMove() {
+        singlePlayer.processComputerCards();
+
+        
+        assertEquals(3, singlePlayer.computerPoints);
+        assertEquals(1, singlePlayer.cards.size()); 
+        assertEquals(1, singlePlayer.computerCards.size()); 
+    }
+
+    @Test
+    public void testProcessComputerCards_invalidMove() {
+        
+        singlePlayer.cards.add(new Card("Blue", 9));
+        singlePlayer.processComputerCards();
+
+        
+        assertEquals(1, singlePlayer.computerCards.size()); 
+    }
+}
 }
